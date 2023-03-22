e028af4a305a1859259ffc6c83efdd83fbe83902
shantanurdubey <shantanu.dubey@nebula-automotive.com>
2023-03-23 00:01:49 +0530

    Update: Issue#2 - Move public methods to private access specifier

--- Logger/Logger/Logger.h
+++ Logger/Logger/Logger.h
@@ -1,11 +1,8 @@
 #pragma once
 #ifndef _LOGGER_H_
 #define _LOGGER_H_
-
 #include <iostream>
 #include <string>
-
-using namespace std;
 
 enum class eLogTypes : std::uint32_t {
     eERROR = 0,
@@ -26,20 +23,22 @@ namespace nLogging
         Logger() {}
         Logger(const Logger& loggerObj) {}
         void operator=(const Logger& loggerObj) {}
+        
+        //Private Methods - To be called from within the class only
+        void error(const char* text);
+        void error(std::string str);
+        void info(const char* text);
+        void info(std::string str);
+        void debug(const char* text);
+        void debug(std::string str);
+        void trace(const char* text);
+        void trace(std::string str);
+        void all(const char* text);
+        void all(std::string str);
     public:
         static Logger* getInstance();
-        void error(const char* text);
-        void error(string str);
-        void info(const char* text);
-        void info(string str);
-        void debug(const char* text);
-        void debug(string str);
-        void trace(const char* text);
-        void trace(string str);
-        void all(const char* text);
-        void all(string str);
         void logMsg(eLogTypes eLogType, const char* text);
-        void logMsg(eLogTypes eLogType, string str);
+        void logMsg(eLogTypes eLogType, std::string str);
         void setLogLevel(eLogTypes eLogLevel);
     };
 }
